256a7eb349cfa3a1b10fe9f0c7cb2b527957776c
DenisPepper <thech@yandex.ru>
2025-11-25 22:48:12 +0300

    Переустановит проект с vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-import devtools from 'solid-devtools/vite';
+import { defineConfig } from 'vite'
+import solid from 'vite-plugin-solid'
 
 export default defineConfig({
-  plugins: [devtools(), solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
-});
+  plugins: [solid()],
+})
