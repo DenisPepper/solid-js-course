f4f7c073a6cf1a8cedf2b0f3ac4a406bdd19ed99
DenisPepper <thech@yandex.ru>
2025-11-29 13:33:58 +0300

    Переименует названия страниц

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Route, Router } from "@solidjs/router";
-import Index from "./pages/Index.jsx";
-import Dinosaur from "./pages/Dinosaur.jsx";
+import Home from "./pages/home-page.jsx";
+import Dinosaur from "./pages/dinosaur-page.jsx";
 import "./App.css";
 
 const App = () => {
   return (
     <Router>
-      <Route path="/" component={Index} />
+      <Route path="/" component={Home} />
       <Route path="/:selectedDinosaur" component={Dinosaur} />
     </Router>
   );
